c89f6b117a42e82d8742bd2630d876fff513da25
luizfvm <vaccari.lf@gmail.com>
2019-11-20 16:58:11 -0300

    Enable CORS

--- src/main/java/com/lf/restfulapi/resources/UserResource.java
+++ src/main/java/com/lf/restfulapi/resources/UserResource.java
@@ -11,6 +11,7 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ import io.swagger.annotations.ApiOperation;
  * UserResource class responsible for implementing HTTP methods.
  */
 
+@CrossOrigin
 @Api(value="User Management System")
 @RestController
 @RequestMapping(value = "/users")
